7b9c6ecc12d417bb1dac17022689c0830cd97681
starry1990 <starry1990@YuedeMBP.home>
2016-06-12 20:31:41 -0400

    Not_finished_code

--- k_means.py
+++ k_means.py
@@ -2,7 +2,13 @@ import numpy as np
 import random as rand
 from matplotlib import pyplot as plt
 from peak_detect import detect_peaks
+from time import time
 
+from sklearn import metrics
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import scale
+from scipy.spatial import distance
 #########################################################
 # The following code apply K-means algorithms to signal
 # by three steps: spike detection, alignment of spikes and k-means algorithm
@@ -12,17 +18,17 @@ from peak_detect import detect_peaks
 # since we use convolution window to find spike, the convolution result may not give
 # us the best estimate
 
-# Question 2: align spikes 
+# Question 2: align spikes: better way to locate the spikes instead of align the maximum? ..
 
 #########################################################
-# Detect_spike function will detect spikes in a signal:
+# process_spike function will detect spikes in input signal and align them :
 # input: 
 # signal: the input signal
 # window_len: the manually set length for window in convolution
 # window_height: the manually set height for window in convolution
 # noise_level: the lower bound parameter in the find local maxima function
 
-# output: the unaligned spike in a 2-D array detected_spikes
+# output: the aligned spikes in a 2-D array detected_spikes
 
 
 def process_spike(signal, window_len, noise_level, window_height=2):
@@ -37,7 +43,7 @@ def process_spike(signal, window_len, noise_level, window_height=2):
 	convolution=convolution/window_len
 
 	# Step 3: find the indices of local maxima of the convolution
-	local_max=detect_peaks(convolution, mph=noise_level*5, mpd=window_len/2, show=True)
+	local_max=detect_peaks(convolution, mph=noise_level*5, mpd=window_len/2, show=False)
 
 	# Step 4: locate/save spike vectors
 	m=len(local_max)
@@ -47,7 +53,7 @@ def process_spike(signal, window_len, noise_level, window_height=2):
 	for item in local_max:
 		detected_spikes[index]=signal[item-window_len/2:item+window_len/2]
 		index=index+1
-		
+
 	# Step 5: align spikes 
 	k=rand.randint(0,m-1)
 	max_location=detected_spikes[k].argmax(axis=0)
@@ -57,6 +63,57 @@ def process_spike(signal, window_len, noise_level, window_height=2):
 		detected_spikes[i]=np.roll(detected_spikes[i],distance)
 
 	return detected_spikes
+
+
+def k_means_spikeDetection(aligned_spikes,num_cluster,iterations=50):
+	
+	# Initialize spikes with lables
+	m=aligned_spikes.shape[0]
+	n=aligned_spikes.shape[1]
+	
+
+	# Take initialize centers
+	k=np.random.permutation(m)
+	initial_center=np.zeros((num_cluster,n))
+	center_vectors_label=np.zeros((num_cluster,n+1))
+	
+	for num in range(num_cluster):
+		initial_center[num]=aligned_spikes[k[num]]
+
+	# Main loop:
+	center_vectors=initial_center
+	for ite in range(1,iterations):
+		# Determine clusters by computing the Eculidean distance
+		clusters_distance=distance.cdist(aligned_spikes,center_vectors,'euclidean',p=2)
+		label=clusters_distance.argmin(axis=1)
+		classified_spikes=np.c_[aligned_spikes,label]
+
+
+		for index in range(0,num_cluster):
+
+			cluster_vector=classified_spikes[classified_spikes[:,-1]==index]
+			
+			number=cluster_vector.shape[0]
+
+			center_vectors_label[index]=1/number*np.sum(cluster_vector,axis=0)
+			
+			center_vectors=np.delete(center_vectors_label,-1,1)
+
+			#print(cluster_vector)
+
+
+	return center_vectors
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- main.py
+++ main.py
@@ -4,11 +4,15 @@ import random as rand
 from matplotlib import pyplot as plt
 from scipy.signal import argrelextrema
 from scipy import signal
+from sklearn import cluster
+
 
 import sys
 sys.path.insert(1, r'./../functions')  # add to pythonpath
-from peak_detect import detect_peaks
+#from peak_detect import detect_spike
 
+from k_means import process_spike
+from k_means import k_means_spikeDetection
 ##################################################################
 # Try for some data: 
 # Try to simulate the compositions of signals from two cells
@@ -17,31 +21,35 @@ from data_initilization_2D import spike_generator
 from data_initilization_2D import waveform_generator
 from data_initilization_2D import noise
 from data_initilization_2D import plot_spike
+#from k_means import detect_spike
 
 
-
-time=1000
-spike_len=20
-lambd=70
-shape_parameter=np.array([[0,2.5],[2,3]])
-shape_parameter2=np.array([[0,4],[2,2]])
+time=50000
+spike_len=200
+lambd=200
+shape_parameter=np.array([[0,30],[30,30]])
+shape_parameter2=np.array([[0,70],[30,50]])
 
 
 
 a=spike_generator(time,spike_len,lambd)
 a2=spike_generator(time,spike_len,lambd)
 
-pl=plot_spike(a)
-
-#plt.plot(pl[0:50])
-
-#plt.show()
 
 b,c=waveform_generator(a,spike_len,shape_parameter)
 b2,c2=waveform_generator(a2,spike_len,shape_parameter2)
 
+
+# plt.plot(c[0:1000])
+# plt.plot(c2[0:1000])
+
+
 d=c+c2
-d=noise(d,0.01)
+d=noise(d,0.02)
+
+# plt.plot(d)
+
+
 
 #plt.plot(d[0:2000])
 # plt.show()
@@ -49,36 +57,97 @@ d=noise(d,0.01)
 d_new=map(abs,d)
 
 
-weights = np.repeat(2, 0.6*spike_len/0.1)
+weights = np.repeat(2, 0.6*spike_len)
 
 con=np.convolve(weights,d_new,'same')
 
 
-con=con/(spike_len/0.1)
+con=con/(spike_len)
 
 #a=argrelextrema(con, np.greater)
-#plt.plot(con[0:2000])
+# plt.plot(con)
+# plt.show()
 
-
-a=detect_peaks(con, mph=0.4, mpd=100, show=False)
+#a=detect_peaks(con, mph=0.4, mpd=100, show=False)
 
 #print(a)
 
 
-i=0
-m=len(a)
-n=spike_len/0.2
+# i=0
+# m=len(a)
+# n=spike_len/0.2
 
-detect_spike=np.zeros(200)
-for item in a:
-	detect_spike=d[item-100:item+100]
-	plt.plot(detect_spike)
-	plt.show()
+# detect_spike=np.zeros((m,n))
+# for item in a:
+# 	detect_spike[i]=d[item-100/2:item+100/2]
+# 	# plt.plot(detect_spike)
+# 	# plt.show()
 
-	i=i+1
+# 	i=i+1
+
+# plt.plot(detect_spike[1])
+# plt.show()
+
+# plt.plot(d)
+# plt.show()
+a=process_spike(d,200,0.1)
+
+b=k_means_spikeDetection(a,3,3)
+
+
+plt.plot(b[0])
+plt.plot(b[1])
+
+plt.show()
+
+
+# x=np.array([1,2,3,4,4,4,4])
+# c=x.argmax(axis=0)
+# print(c)
+# plt.plot(a[1])
+# plt.plot(a[2])
+
+# plt.plot(a[3])
+# plt.plot(a[4])
 
 
 
+# plt.plot(a[5])
+# plt.plot(a[6])
+
+
+# plt.plot(a[7])
+# plt.plot(a[8])
+
+
+#plt.show()
+
+
+
+
+
+# data=a
+# k = 2
+# kmeans = cluster.KMeans(n_clusters=k)
+# kmeans.fit(data)
+
+# labels = kmeans.labels_
+# centroids = kmeans.cluster_centers_
+
+# for i in range(k):
+#     # select only data observations with cluster label == i
+#     ds = data[np.where(labels==i)]
+#     # plot the data observations
+#     plt.plot(ds[:,0],ds[:,1],'o')
+#     # plot the centroids
+#     lines = plt.plot(centroids[i,0],centroids[i,1],'kx')
+#     # make the centroid x's bigger
+#     plt.setp(lines,ms=15.0)
+#     plt.setp(lines,mew=2.0)
+# plt.show()
+
+
+# tel = {'jack': 4098, 'sape': 4139}
 #print(a)
 #print(con[0:1000])
 
